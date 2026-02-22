50cd2fa86d7111d9e112de9ec562749f55b1713e
Yunus Emre Tom <yunusemretom@gmail.com>
2026-02-22 11:47:06 +0300

    Remove ignored files

--- build/object_detection/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tom/Documents/Dog_Fight/install/object_detection'
--- ros2_ws/build/object_detection/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tom/Documents/Dog_Fight/ros2_ws/install/object_detection'
